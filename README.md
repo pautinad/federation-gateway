599dfd37b7aff92c937c6a8c66eaf12b06ee7577
pautina_d@mail.ru <P@cii--22373>
2021-09-03 10:18:11 +0400

    add federation gateway

---
+++ README.md
@@ -0,0 +1,3 @@
+# federation-gateway
+1. run `npm i` to install dependencies
+2. run command `npm run gateway` or run button from debug tab in vscode
---
+++ gateway.js
@@ -0,0 +1,73 @@
+import {ApolloGateway, RemoteGraphQLDataSource} from "@apollo/gateway";
+import {ApolloServer} from "apollo-server";
+import {ApolloServerPluginSchemaReporting, ApolloServerPluginUsageReporting, ApolloServerPluginLandingPageGraphQLPlayground} from "apollo-server-core";
+
+
+const gateway = new ApolloGateway({
+    serviceList: [
+        {name: 'shows', url: 'http://localhost:4001/graphql'},
+        {name: 'reviews', url: 'http://localhost:4002/graphql'},
+        {name: 'samples', url: 'http://localhost:8080/graphql'}
+    ],
+    buildService({name, url}) {
+        return new RemoteGraphQLDataSource({
+            url, willSendRequest(request, context) {
+                if (context === undefined
+                    || context.customHeaders === undefined
+                    || context.customHeaders.headers === undefined) {
+                    return;
+                }
+                const customHeaders = context.customHeaders.headers;
+
+                Objects.keys(customHeaders).map(key => {
+                    request.http && request.http.headers.set(key, customHeaders[key])
+                });
+
+            }
+        });
+    }
+});
+
+const server = new ApolloServer({
+    gateway,
+    subscriptions: false,
+    apollo: {
+        key: 'service:My-Graph-vfy4m6:THSHKlHL1hSxteml7Lrn_Q',
+        graphId: 'My-Graph-vfy4m6',
+        graphVariant: 'current',
+        schemaReporting: true
+    },
+    plugins: [
+        ApolloServerPluginUsageReporting({
+            sendHeaders: {all: true},
+            sendVariableValues: {all: true},
+        }),
+        ApolloServerPluginLandingPageGraphQLPlayground({
+            settings: {
+              'some.setting': true,
+              'general.betaUpdates': false,
+              'editor.theme': 'dark',
+              'editor.cursorShape': 'line',
+              'editor.reuseHeaders': true,
+              'tracing.hideTracingResponse': true,
+              'queryPlan.hideQueryPlanResponse': true,
+              'editor.fontSize': 14,
+              'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
+              'request.credentials': 'omit',
+            },
+          }),
+    ],
+    context: req => ({
+        req,
+        customHeaders: {
+            headers: {
+                ...req.headers,
+            },
+        },
+    }),
+    playground: false,
+});
+
+await server.listen().then(info => {
+    console.log(`Server started at - ${info.url}`);
+});
